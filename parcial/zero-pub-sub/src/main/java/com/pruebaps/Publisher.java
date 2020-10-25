6314b259e705ac808298d2bf361c440fb2c9c2d7
juanAngaritaweb <juanAngaritaweb@hotmail.com>
2020-10-24 21:38:57 -0500

    agregando parte logica\

--- parcial/zero-pub-sub/src/main/java/com/pruebaps/Publisher.java
+++ parcial/zero-pub-sub/src/main/java/com/pruebaps/Publisher.java
@@ -1,10 +1,20 @@
 package com.pruebaps;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+
+import model.Artist;
+import model.Crick;
+
 import org.zeromq.ZContext;
+import java.util.Scanner;
 
 //
 // Patron distribucion one-way data, Publicador Suscriptor
@@ -13,10 +23,25 @@ import org.zeromq.ZContext;
 //  Ata el socket PUB a tcp://*:5556
 //  Publica actualizaciones del clima aleatorias
 //
+
 public class Publisher
 {
     public static void main(String[] args) throws Exception
     {
+    	int auxId = 1;
+    	Scanner sc = new Scanner(System.in);
+    	List<Artist> artistas = new ArrayList<>();
+    	System.out.println("BIENVENIDO A CRICKER");
+    	
+    	
+    	try {
+    	      FileWriter myWriter = new FileWriter("Artistas.txt");
+    	      myWriter.close();
+    	}catch(IOException e) {
+    	      System.out.println("An error occurred.");
+    	}
+    	
+    	
         //Establece el ambiente o contexto zeromq
         try (ZContext context = new ZContext()) {
             //Crea un socket tipo PUB
@@ -25,21 +50,90 @@ public class Publisher
             publisher.bind("tcp://*:5556");
             publisher.bind("ipc://weather");
 
-            //  Inicializa los numeros al azar
-            Random srandom = new Random(System.currentTimeMillis());
             while (!Thread.currentThread().isInterrupted()) {
-                //  Obtiene los valores
-                int zipcode, temperature, relhumidity;
-                zipcode = 10000 + srandom.nextInt(10000);
-                temperature = srandom.nextInt(215) - 80 + 1;
-                relhumidity = srandom.nextInt(50) + 10 + 1;
+            	
+            	System.out.println("1. Registrar nuevo artista");
+            	System.out.println("2. Enviar mensaje a fans");
+            	
+            	int opcion = sc.nextInt();
+            	sc.nextLine();
+            	
+            	if(opcion == 1) {
+            		//ingreso de datos y creo el artista
+            		System.out.println("Ingresa tus datos");
+                	System.out.print("Nombre: ");
+                	String nombre = sc.nextLine();
+                	Artist artist = new Artist();
+                	artist.setName(nombre);
+                	artist.setId(auxId);
+                	artistas.add(artist);
+                	auxId++;
+                	
+                	//Agrego el artista a un archivo de texto para tener pesistencia
+                    try {
+                        FileWriter writer = new FileWriter("Artistas.txt", true);
+                        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+             
+                        bufferedWriter.write(nombre);
+                        bufferedWriter.newLine();
+             
+                        bufferedWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+             
+                    
+                	String nuevoArtista = String.format(
+                            "%s %s", "Artistas", artist.getName()
+                        );
+                	System.out.println(nuevoArtista);
+                     //Envia el mensaje a todos los suscriptores
+                     publisher.send(nuevoArtista, 0);
+                	
+                	System.out.println("Ahora " + nombre + " puede publicar mensajes");
+                }
+                
+            	else if(opcion == 2) {
+            		System.out.println("Artistas");
+            		System.out.print("Id   ");
+            		System.out.println("Nombre");
+            		for(Artist a: artistas) {
+            			System.out.print(a.getId()+"   ");
+            			System.out.println(a.getName());
+            		}
+            		System.out.println("Seleccionar artista(ID): ");
+            		int id;
+            		id = sc.nextInt();
+            		Artist artist = new Artist();
+            		for(Artist a: artistas) {
+            			if(a.getId() == id) {
+            				artist = a;
+            			}
+            		}
+                	System.out.println("Que mensaje le quieres compartir hoy " + artist.getName() + " a tus seguidores?");
+                	sc.nextLine();
 
-                // Crea el mensaje en cadena de caracteres
-                String update = String.format(
-                    "%05d %d %d", zipcode, temperature, relhumidity
-                );
-                //Envia el mensaje a todos los suscriptores
-                publisher.send(update, 0);
+                	String mensaje;
+                	mensaje = sc.nextLine();
+                	Crick crick = new Crick();
+                	crick.setArtist(artist);
+                	crick.setMessage(mensaje);
+                	                	
+                	//System.out.println("Publicando");
+                	//System.out.println(crick.getMessage());
+                	//System.out.println(crick.getArtist().getName());
+
+                	String mensajeCrick = String.format(
+                            "%s %s", crick.getArtist().getName(), crick.getMessage()
+                        );
+                	System.out.println(mensajeCrick);
+                        //Envia el mensaje a todos los suscriptores
+                        publisher.send(mensajeCrick, 0);
+            	}else {
+            		System.out.println("Opcion no valida");
+            	}
+            	
+                
             }
         }
     }
--- parcial/zero-pub-sub/src/main/java/com/pruebaps/Subscriber.java
+++ parcial/zero-pub-sub/src/main/java/com/pruebaps/Subscriber.java
@@ -1,5 +1,11 @@
 package com.pruebaps;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import org.zeromq.SocketType;
@@ -17,45 +23,81 @@ public class Subscriber
 {
     public static void main(String[] args)
     {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	List<String> mensajes = new ArrayList<>();
+    	
+    	
         //Establece el ambiente o contexto zeromq
         try (ZContext context = new ZContext()) {
             //  Socket to talk to server
-            System.out.println("Collecting updates from weather server");
-            //Crea un socket tipo SUB
+            System.out.println("Bienvenido a Cricker");
             ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
             //Conecta el socket a un puerto
             //subscriber.connect("tcp://localhost:5556");
             //Prueba red domestica
-            subscriber.connect("tcp://192.168.0.14:5556");
+            subscriber.connect("tcp://192.168.0.10:5556");
             
-            String filter = (args.length > 0) ? args[0] : "10001 ";
+            String filter = (args.length > 0) ? args[0] : "Artistas ";
             //Se suscribe con codigo especial que le permitira filtar los mensajes del publicador
             subscriber.subscribe(filter.getBytes(ZMQ.CHARSET));
-
-            //  Procesa 100 actualizaciones
-            int update_nbr;
-            long total_temp = 0;
-            for (update_nbr = 0; update_nbr < 100; update_nbr++) {
-                //Recibe el mensaje en cadena de caracteres
-                //Remueve el caracter '0' de la cola
-                String string = subscriber.recvStr(0).trim();
+            
+            //crear el hilo
+            //el hilo se queda leyendo
+            //guardo lo que me llegue a "mensajes"
+            
+            
+            while(true) {
+                System.out.println("Elige una opcion");
+                System.out.println("1. Suscribirme a un artista");
+                System.out.println("2. Ver mensajes");
                 
-                //Saca el mensaje
-                StringTokenizer sscanf = new StringTokenizer(string, " ");
-                int zipcode = Integer.valueOf(sscanf.nextToken());
-                int temperature = Integer.valueOf(sscanf.nextToken());
-                int relhumidity = Integer.valueOf(sscanf.nextToken());
-
-                total_temp += temperature;
+                int opcion = sc.nextInt();
+                sc.nextLine();
+                if(opcion == 1) {
+                	System.out.println("Estos son los artistas a los que te puedes subscribir");
+                	
+                	//lectura del archivo
+                	try {
+                        FileReader reader = new FileReader("Artistas.txt");
+                        BufferedReader bufferedReader = new BufferedReader(reader);
+             
+                        String line;
+             
+                        while ((line = bufferedReader.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                        reader.close();
+             
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                	
+                	System.out.println("Me quiero subscribir a:");
+                	String artista = sc.nextLine();
+                	filter = artista;
+                    subscriber.subscribe(filter.getBytes(ZMQ.CHARSET));
+                }else if(opcion == 2) {
+                	while(true) {
+                		String string = subscriber.recvStr(0).trim();
+                        
+                		//for de todos los "mensajes"
+                        StringTokenizer sscanf = new StringTokenizer(string, " ");
+                        String nombreArtista= sscanf.nextToken();
+                        String mensaje = sscanf.nextToken();
+                        if(nombreArtista.compareTo("Artistas") == 0) {
+                        	System.out.println("HAY UN NUEVO ARTISTA AL QUE TE PUEDES SUBSCRIBIR");
+                        	System.out.println(mensaje);
+                        }else {
+                        	 System.out.println("Nuevo mensaje");
+                             System.out.println("Artista: " + nombreArtista);
+                             System.out.println("Mensaje: " + mensaje);
+                        }
+                	}
+                }	
             }
 
-            System.out.println(
-                String.format(
-                    "Average temperature for zipcode '%s' was %d.",
-                    filter,
-                    (int)(total_temp / update_nbr)
-                )
-            );
         }
     }
 }
